46cadc455ba96ca07a04619c00c637c549913e1b
Eidan4 <eidanjoelruizpinzon@gmail.com>
2022-06-21 21:40:16 -0500

    NodeAPI

---
+++ db/migrations/20220525202516-order.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const {OrderSchema,ORDER_TABLE}= require('./../models/order.model');
+const { DataTypes, Sequelize } = require('sequelize');
+const {CUSTOMER_TABLE}= require('./../models/customer.model');
+
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.createTable(ORDER_TABLE,{
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey:true,
+        type:DataTypes.INTEGER,
+      },
+      customerId: {
+       field:'customer_id',
+       allowNull: false,
+       type:DataTypes.INTEGER,
+       reference:{
+         model:CUSTOMER_TABLE,
+         key:'id'
+       }
+      },
+      createdAt:{
+        allowNull: false,
+        type: DataTypes.DATE,
+        field:'created_at',
+        defaultValue:Sequelize.NOW,
+      }
+    });
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable(OrderSchema);
+  }
+};
